6a5d1053aea5220694fcfab50d0ff7263bdcdf6f
Kaua reis <kauadragon036@gmail.com>
2025-04-05 02:09:58 -0300

    commit da página web de vendas da nike

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let body = document.querySelector("body")
+let tenis = document.querySelector(".imagem-tenis")
+//document = html
+// querySelector = pegador/selecionador
+
+function mudarVisual(cor, imagem) {
+
+    tenis.src = imagem
+    body.style.background = cor
+}
+//!Palavra chave 3: Conquista
